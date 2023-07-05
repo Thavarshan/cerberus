c4a561cc89d1b2c60cee2f708c68d764e36e5ded
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2023-07-05 12:44:22 +0100

    Update dependencies

--- bin/upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-STRING="README.md"
-shopt -s nullglob
-for dir in ./*; do
-    if [[ $dir != *"$STRING"* ]]; then
-        cd $dir && npx -yes -p npm-check-updates ncu -u && yarn install && cd ../..
-    fi
-done
